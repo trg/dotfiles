b08f5dcb83cd714f137881899228abbefd7086e0
tgraft <tgraft@ebay.com>
2016-09-07 13:41:40 -0400

    nerdtree

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
+#!/bin/sh
+
 ln -nfs ~/dotfiles/vim/vimrc ~/.vimrc
 ln -nfs ~/dotfiles/vim/vim ~/.vim
+curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
+reset
+apt-vim install -y https://github.com/scrooloose/nerdtree.git
+
